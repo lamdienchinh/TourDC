c23cb040c84a14022cfbc80e822a5d9284b9fc68
duync2006 <reynold.cs.w3@gmail.com>
2023-07-17 09:11:10 +0700

    change struct file

--- SC/scripts/deploy.js
+++ SC/scripts/deploy.js
@@ -37,7 +37,7 @@ async function main() {
 
 async function saveFrontendFiles(token) {
   const fs = require("fs");
-  const contractsDir = path.join(__dirname, "..", "frontend", "src", "contracts");
+  const contractsDir = path.join(__dirname, "..", '..', "FE", "src", "contracts");
 
   if (!fs.existsSync(contractsDir)) {
     fs.mkdirSync(contractsDir);
