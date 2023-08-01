c7000170e418c8751dd513beda033b401f18af66
Chinh Lâm Điền <lamdienchinhtd9a2@gmail.com>
2023-08-01 17:40:07 +0700

    Modified UI

--- FE/src/pages/QRcode/qrCode.js
+++ FE/src/pages/QRcode/qrCode.js
@@ -10,35 +10,37 @@ import "./qrCode.scss";
 
 const QrCode = () => {
   return (
-    <Container className="mb-5 mt-5">
-      <div className="qr-code">
-        <Row>
-          <Col xs={6} md={4}>
-            <Image src={QRcode_1} thumbnail />
-          </Col>
-          <Col xs={6} md={4}>
-            <Image src={QRcode_2} thumbnail />
-          </Col>
-          <Col xs={6} md={4}>
-            <Image src={QRcode_3} thumbnail />
-          </Col>
-        </Row>
-        <Row className="justify-content-md-center">
-          <Col xs={6} md={4}>
-            {" "}
-            Place ID 1
-          </Col>
-          <Col xs={6} md={4}>
-            {" "}
-            Place ID 2
-          </Col>
-          <Col xs={6} md={4}>
-            {" "}
-            Place ID 3
-          </Col>
-        </Row>
-      </div>
-    </Container>
+    <div className="Qrcode">
+      <Container className="mb-5 mt-5">
+        <div className="qr-code">
+          <Row>
+            <Col xs={6} md={4}>
+              <Image src={QRcode_1} thumbnail />
+            </Col>
+            <Col xs={6} md={4}>
+              <Image src={QRcode_2} thumbnail />
+            </Col>
+            <Col xs={6} md={4}>
+              <Image src={QRcode_3} thumbnail />
+            </Col>
+          </Row>
+          <Row className="justify-content-md-center">
+            <Col xs={6} md={4}>
+              {" "}
+              Place ID 1
+            </Col>
+            <Col xs={6} md={4}>
+              {" "}
+              Place ID 2
+            </Col>
+            <Col xs={6} md={4}>
+              {" "}
+              Place ID 3
+            </Col>
+          </Row>
+        </div>
+      </Container>
+    </div>
   );
 };
 
